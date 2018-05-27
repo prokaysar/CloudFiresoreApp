43181fe566034b57584a7b70556fbb6670c063d7
kawsar <kawsar_dc@outlook.com>
2018-05-28 03:27:55 +0600

    Adding Data

---
+++ app/src/main/java/me/prokawsar/cloudfiresoreapp/MainActivity.java
@@ -0,0 +1,110 @@
+package me.prokawsar.cloudfiresoreapp;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private EditText nameText,brandText,descText,prizeText,qualityText;
+    private Button saveButton;
+    private FirebaseFirestore mDatabase;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameText = findViewById(R.id.name_id);
+        brandText = findViewById(R.id.brand_id);
+        descText = findViewById(R.id.desc_id);
+        prizeText = findViewById(R.id.prize_id);
+        qualityText = findViewById(R.id.quality_id);
+        saveButton = findViewById(R.id.save_id);
+
+        mDatabase = FirebaseFirestore.getInstance();
+
+        saveButton.setOnClickListener(this);
+
+
+    }
+    private boolean validateInputs(String name, String brand, String desc, String price, String qty) {
+        if (name.isEmpty()) {
+            nameText.setError("Name required");
+            nameText.requestFocus();
+            return true;
+        }
+
+        if (brand.isEmpty()) {
+            brandText.setError("Brand required");
+            brandText.requestFocus();
+            return true;
+        }
+
+        if (desc.isEmpty()) {
+            descText.setError("Description required");
+            descText.requestFocus();
+            return true;
+        }
+
+        if (price.isEmpty()) {
+            prizeText.setError("Price required");
+            prizeText.requestFocus();
+            return true;
+        }
+
+        if (qty.isEmpty()) {
+            qualityText.setError("Quantity required");
+            qualityText.requestFocus();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        String name = nameText.getText().toString().trim();
+        String brand = brandText.getText().toString().trim();
+        String desc = descText.getText().toString().trim();
+        String price = prizeText.getText().toString().trim();
+        String qty = qualityText.getText().toString().trim();
+
+        if (!validateInputs(name,brand,desc,price,qty)){
+
+            CollectionReference dbProducts = mDatabase.collection("Products");
+            Products products = new Products(
+                    name,
+                    brand,
+                    desc,
+                    Double.parseDouble(price),
+                    Integer.parseInt(qty)
+            );
+            dbProducts.add(products)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+
+                            Toast.makeText(MainActivity.this, "New Products are added.", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+
+    }
+}
---
+++ app/src/main/java/me/prokawsar/cloudfiresoreapp/Products.java
@@ -0,0 +1,38 @@
+package me.prokawsar.cloudfiresoreapp;
+
+public class Products {
+    private String name,brand,desc;
+    private double price;
+    private int qty;
+
+    public Products() {
+    }
+
+    public Products(String name, String brand, String desc, double price, int qty) {
+        this.name = name;
+        this.brand = brand;
+        this.desc = desc;
+        this.price = price;
+        this.qty = qty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+}
